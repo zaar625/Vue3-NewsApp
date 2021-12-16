564b1053bbe7d2b0aa96bb77f3704f4670726c76
zaar625 <zaar625@naver.com>
2021-12-16 15:22:56 +0900

    스타일링- (각 컴포넌트 마다 필요한 API 데이터 받아오기)

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,6 +1,8 @@
 <template>
 <div>
-  <div v-for="item in askItem" >{{item.title}}</div>
+  <p v-for="item in askItem" >
+    <a :href="item.url">{{item.title}}</a>
+  </p>
 </div>
 </template>
 
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
-  <div v-for="user in this.$store.state.news">{{user.title}}</div>
+  <p v-for="item in this.$store.state.news">
+    <a v-bind:href="item.url">{{item.title}}</a>
+    <small>{{item.time_ago}} by {{item.user}}</small>  
+  </p>
 </div>
 </template>
 
