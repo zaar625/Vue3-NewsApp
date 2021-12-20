461ca19406e4eb48c644d9f11d6865b4473ceec7
zaar625 <zaar625@naver.com>
2021-12-20 23:20:28 +0900

    하이오더 컴포넌트 생성으로  필요없는 파일(ASK/Jobs/News) 및 action.js 내부 정리

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,44 +1,44 @@
 import {fetchJobsList, fetchNewsList,fetchAskList, fetchUserInfo, fetchCommentItem,fetchList} from '../api/index';
 
 export default{
-    FECH_NEWS(context){
-        fetchNewsList()
-        .then(response=>{
-            // console.log(response);
-            context.commit('SET_NEWS',response.data);
-            return response;
-        })
-        .catch(error=>{
-            console.log(error);
-        })
-    },
-    FECH_JOBS(context){
-        fetchJobsList()
-        .then(response=>{
-            context.commit('SET_JOBS', response.data)
-        })
-        .catch(error=>{
-            console.log(error);
-        })
-    },
-    FETCH_ASK(context){
-        fetchAskList()
-        .then(response=>{
-            context.commit('SET_ASK', response.data)
-        })
-        .catch(error=>{
-            console.log(error);
-        })
-    },
-    FETCH_USER({commit},name){
-        fetchUserInfo(name)
-        .then(({data})=>{
-            commit('FETCH_USER', data)
-        })
-        .catch(error=>{
-            console.log(error);
-        })
-    },
+    // FECH_NEWS(context){
+    //     fetchNewsList()
+    //     .then(response=>{
+    //         // console.log(response);
+    //         context.commit('SET_NEWS',response.data);
+    //         return response;
+    //     })
+    //     .catch(error=>{
+    //         console.log(error);
+    //     })
+    // },
+    // FECH_JOBS(context){
+    //     fetchJobsList()
+    //     .then(response=>{
+    //         context.commit('SET_JOBS', response.data)
+    //     })
+    //     .catch(error=>{
+    //         console.log(error);
+    //     })
+    // },
+    // FETCH_ASK(context){
+    //     fetchAskList()
+    //     .then(response=>{
+    //         context.commit('SET_ASK', response.data)
+    //     })
+    //     .catch(error=>{
+    //         console.log(error);
+    //     })
+    // },
+    // FETCH_USER({commit},name){
+    //     fetchUserInfo(name)
+    //     .then(({data})=>{
+    //         commit('FETCH_USER', data)
+    //     })
+    //     .catch(error=>{
+    //         console.log(error);
+    //     })
+    // },
     FETCH_ITEM({commit},id){
         fetchCommentItem(id)
         .then(({data})=>{
